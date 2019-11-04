64cdb128d57ddd1badb47b63edfce9402ff6e609
Alexey <js.dudko@gmail.com>
2019-11-04 22:11:12 +0300

    Filtration added

--- filter/FilteredSortableList.js
+++ filter/FilteredSortableList.js
@@ -9,43 +9,47 @@ var FilteredSortableList = React.createClass({
 
     getInitialState: function() {
         return {
-            placeholder: 'Всё под контролем',
+            filterStr: '',
             sortFlag: this.props.startSortFlag,
             words: this.props.inputWords,
         };
     },
 
-
     update: function(EO) {
         console.log('Теперь фильтр такой: ' + EO.target.value);
-        this.setState( {placeholder: EO.target.value} );
+        this.setState( {filterStr: EO.target.value} );
     },
 
     sortWords: function(EO) {
         this.setState( {sortFlag: !this.state.sortFlag} );
-        console.log('Фильтрация: ' + (EO.target.checked ? 'включена' : 'выключена') );
+        console.log('Сортировка ' + (EO.target.checked ? 'включена' : 'выключена') );
     },
 
     reset: function(EO) {
         console.log('Сброс к начальному состоянию');
-        // this.setState( {placeholder: ''} );
         this.setState( this.getInitialState() );
     },
 
     render: function() {
 
         var optionElems = this.state.words.map( v =>
-            React.DOM.option({key:v.code, value:v.text}, v.text),
+            React.DOM.option({key:v.code, value:v.text, disabled: 'true'}, v.text),
         );
 
         if (this.state.sortFlag)
-            optionElems.sort( (item1, item2) => item1.props.value > item2.props.value );
+            optionElems.sort( (currItem, nextItem) => currItem.props.value > nextItem.props.value );
+        
+        if (this.state.filterStr) {
+            var re = new RegExp(this.state.filterStr);
+            optionElems = optionElems.filter( currItem => re.test(currItem.props.value) );
+        }
+            
         
         return React.DOM.div( {className:'AwesomeList'},
             React.DOM.div( {className:'controlsGroup'},  
                 React.DOM.input( {type: 'checkbox', name: 'sorting', className: 'sortOpt', checked: this.state.sortFlag||false,
                     onChange: this.sortWords} ),
-                React.DOM.input( {type: 'text', name: 'filtering', className: 'filterString', value: this.state.placeholder,
+                React.DOM.input( {type: 'text', name: 'filtering', className: 'filterString', value: this.state.filterStr,
                     onChange: this.update} ),
                 React.DOM.input( {type: 'button', className: 'resetBtn', defaultValue: 'Сброс',
                     onClick: this.reset} ),
